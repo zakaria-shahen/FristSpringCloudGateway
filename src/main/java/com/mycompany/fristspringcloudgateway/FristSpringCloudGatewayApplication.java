be8580371a541480944b9fa022cc3bf187b7934a
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-11 21:52:22 +0200

    add enable Eureka and discovery clients

--- src/main/java/com/mycompany/fristspringcloudgateway/FristSpringCloudGatewayApplication.java
+++ src/main/java/com/mycompany/fristspringcloudgateway/FristSpringCloudGatewayApplication.java
@@ -2,8 +2,13 @@ package com.mycompany.fristspringcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.factory.AddRequestHeaderGatewayFilterFactory;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class FristSpringCloudGatewayApplication {
 
 	public static void main(String[] args) {
