37e118d60db8bdf6fdcb8a2a1fa46510cdb1714c
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-11 18:01:37 +0200

    init

---
+++ src/main/java/com/mycompany/fristspringcloudgateway/FristSpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.fristspringcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristSpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristSpringCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/fristspringcloudgateway/FristSpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.fristspringcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristSpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
